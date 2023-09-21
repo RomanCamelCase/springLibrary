97fe0a90cbcda6d8451671ae8a52712dfe557b35
DrunkSun <roman.katsaran@nure.ua>
2023-09-21 23:53:55 +0200

    Added user DAO

--- src/main/java/com/gmail/romkatsis/validators/UserValidator.java
+++ src/main/java/com/gmail/romkatsis/validators/UserValidator.java
@@ -1,6 +1,8 @@
 package com.gmail.romkatsis.validators;
 
+import com.gmail.romkatsis.dao.UserDAO;
 import com.gmail.romkatsis.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -9,6 +11,13 @@ import java.time.LocalDate;
 
 @Component
 public class UserValidator implements Validator {
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserValidator(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
     @Override
     public boolean supports(Class<?> clazz) {
         return User.class.equals(clazz);
@@ -17,6 +26,11 @@ public class UserValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         User user = (User) target;
+
+        if (userDAO.getUser(user.getFullName()).isPresent()) {
+            errors.rejectValue("fullName", "", "We already have a user with this full name");
+        }
+
         int currentYear = LocalDate.now().getYear();
         if (currentYear - user.getBirthYear()  < 18) {
             errors.rejectValue("birthYear", "", "You are too young");
